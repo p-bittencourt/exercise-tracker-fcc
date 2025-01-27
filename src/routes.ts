131ea3d8f35d97cc234c1ddaaa1cea26e81583b8
pedro <pedromonteirobittencourt@gmail.com>
2025-01-27 11:34:49 -0300

    fixed typo on GET /api/users

--- src/routes.ts
+++ src/routes.ts
@@ -13,7 +13,7 @@ export const createRoutes = (app: Application) => {
 
   app.get('/api/users', (req: Request, res: Response) => {
     const output = users.map(
-      (user) => `"_id:${user._id}, "username":${user.username}`
+      (user) => `"_id":${user._id}, "username":${user.username}`
     );
     res.send(output);
   });
